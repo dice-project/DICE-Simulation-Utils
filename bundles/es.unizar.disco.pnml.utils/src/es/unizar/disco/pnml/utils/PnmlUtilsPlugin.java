be5eb379eb0824f6afbe5ab019dbc385182fae31
Abel Gómez <abgolla@gmail.com>
2021-01-13 19:24:31 +0100

    Add license to source files

--- bundles/es.unizar.disco.pnml.utils/src/es/unizar/disco/pnml/utils/PnmlUtilsPlugin.java
+++ bundles/es.unizar.disco.pnml.utils/src/es/unizar/disco/pnml/utils/PnmlUtilsPlugin.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2021 DisCo Group - Universidad de Zaragoza.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 1.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-1.0/
+ *
+ * SPDX-License-Identifier: EPL-1.0
+ *
+ * Contributors:
+ *     Abel Gómez
+ *******************************************************************************/
 package es.unizar.disco.pnml.utils;
 
 import org.eclipse.jface.resource.ImageDescriptor;
